660ae7767a63a09a3fcdf299d5110348a8b6b2f7
murilohenzo <murilohezo@gmail.com>
2024-03-29 10:16:51 -0300

    Adiciona estrutura do projeto

---
+++ compra-orquestrada/src/main/java/br/com/murilohenzo/compraorquestrada/CompraOrquestradaApplication.java
@@ -0,0 +1,13 @@
+package br.com.murilohenzo.compraorquestrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompraOrquestradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraOrquestradaApplication.class, args);
+	}
+
+}
